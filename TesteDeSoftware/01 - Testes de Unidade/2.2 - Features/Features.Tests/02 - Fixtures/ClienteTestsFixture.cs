de8e71b9b09e49d6aedc0087ddc63ac47e6ecb9f
elitonlunardi <lunardi.eliton@gmail.com>
2019-09-22 17:19:23 -0300

    mock

--- TesteDeSoftware/01 - Testes de Unidade/2.2 - Features/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
+++ TesteDeSoftware/01 - Testes de Unidade/2.2 - Features/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Bogus;
 using Bogus.DataSets;
 using Features.Clientes;
@@ -14,30 +16,45 @@ namespace Features.Tests
 
     public class ClienteTestsFixture : IDisposable
     {
-        public Cliente GerarClienteValido()
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
         {
             var genero = new Faker().PickRandom<Name.Gender>();
 
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente
+                (
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                    "",
+                    ativo,
+                    DateTime.Now
+                ))
+                .RuleFor(c => c.Email, (f, c) => f.Internet.Email
                     (
-                        Guid.NewGuid(),
-                        f.Name.FirstName(genero),
-                        f.Name.LastName(genero),
-                        f.Date.Past(80, DateTime.Now.AddYears(-18)),
-                        "",
-                        true,
-                        DateTime.Now
-                    ))
-                .RuleFor(c => c.Email, (f, c) => f.Internet.Email(
-                    c.Nome.ToLower(),
-                    c.Sobrenome.ToLower()));
-            return cliente;
+                        c.Nome.ToLower(),
+                        c.Sobrenome.ToLower())
+                );
+            return clientes.Generate(quantidade);
         }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public Cliente GerarClienteValido() =>
+            GerarClientes(1, true).FirstOrDefault();
 
         public Cliente GerarClienteInvalido()
         {
-            //teste
             var genero = new Faker().PickRandom<Name.Gender>();
 
             var cliente = new Faker<Cliente>("pt_BR")
@@ -53,9 +70,6 @@ namespace Features.Tests
             return cliente;
         }
 
-        public void Dispose()
-        {
-
-        }
+        public void Dispose() { }
     }
 }
---
+++ TesteDeSoftware/01 - Testes de Unidade/2.2 - Features/Features.Tests/04 - Mock/ClienteServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+using Features.Clientes;
+using MediatR;
+using Moq;
+using Moq.AutoMock;
+using Xunit;
+
+namespace Features.Tests
+{
+    [Collection(nameof(ClienteCollection))]
+    public class ClienteServiceTests
+    {
+        private readonly ClienteTestsFixture _clienteTestsFixture;
+
+        public ClienteServiceTests(ClienteTestsFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Adicionar cliente com sucesso")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveExecutarComSucesso()
+        {
+            //Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            //Act
+            clienteService.Adicionar(cliente);
+
+            //Assert
+            Assert.True(cliente.EhValido());
+            clienteRepo.Verify(rep => rep.Adicionar(cliente), Times.Once);
+            mediatr.Verify(med => med.Publish(It.IsAny<INotification>(),CancellationToken.None), Times.Once);
+        }
+
+
+        [Fact(DisplayName = "Adicionar cliente com falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            //Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            //Act
+            clienteService.Adicionar(cliente);
+
+            //Assert
+            Assert.False(cliente.EhValido());
+            clienteRepo.Verify(rep => rep.Adicionar(cliente), Times.Never);
+            mediatr.Verify(med => med.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+    }
+}
